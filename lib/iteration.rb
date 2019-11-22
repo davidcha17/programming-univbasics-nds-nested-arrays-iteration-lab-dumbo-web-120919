128b53b4822abf260b60b9b483ae4052d6b59875
David Cha <david.cha44@qmail.cuny.edu>
2019-11-22 20:55:44 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,7 +11,7 @@ def join_ingredients(src)
   i = 0
   while i < src.length do
     inner = src[i]
-    result << "I love #{inner[0]} and #{inner[1]} on my pizza"
+    result << "I love #{inner[i]} and #{inner[i+1]} on my pizza"
     i += 1
   end
   result
